497c5a1bf88c14a9c0e559f6f882109f076973ae
Vy Tran <uyenvy071097@gmail.com>
2023-08-27 15:26:01 +0200

    add CORS

--- src/main/java/com/example/weather/WeatherController.java
+++ src/main/java/com/example/weather/WeatherController.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/weather")
+@CrossOrigin(origins = "http://localhost:4200")
 public class WeatherController {
 
     @GetMapping("/")
